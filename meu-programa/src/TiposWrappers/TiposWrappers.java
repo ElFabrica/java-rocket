462727632fdc374cfb3e410484585f724ae9161d
Arthur Fabricyo <142946955+ElFabrica@users.noreply.github.com>
2026-05-25 20:49:47 -0300

    Add files via upload

---
+++ meu-programa/src/TiposWrappers/TiposWrappers.java
@@ -0,0 +1,21 @@
+package TiposWrappers;
+
+import java.io.File;
+
+public class TiposWrappers {
+    public static void main(String[] args){
+        /*double numberDouble = 123.56;
+        int numberInt = (int) numberDouble;
+        System.out.println((numberInt)); */
+
+        /*
+        Integer obj1 = Integer.valueOf( "100");
+        Integer obj2 = Integer.valueOf("102");
+
+        System.out.println(obj1.equals((obj2)));
+        System.out.println(obj2.compareTo((obj1))); */
+
+        File math = null;
+
+    }
+}
