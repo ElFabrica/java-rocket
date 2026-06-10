f7100513ccc7712d4f4d88b74ef9890a0b29f67c
Arthur <arthur.fabricyo@gmail.com>
2026-06-10 00:35:52 -0200

    testing arrays

---
+++ meu-programa/src/je_19_arrays/Arrays.java
@@ -0,0 +1,22 @@
+package je_19_arrays;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+    Integer [] array = new Integer[] {2,4,10,5,15,3};
+/*
+    array [0] = 2;
+    array   [1] = 4;
+    array   [2] = 10;
+    array   [3] = 5;
+    array   [4] = 15;
+    array   [5] = 3;
+
+ */
+        for (int i = 0; i < 6; i++) {
+            System.out.println(array[i]);
+        }
+}
+}
+
+
